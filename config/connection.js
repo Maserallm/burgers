390e6eae7ffd220e0223a07f795d4938073e7ef5
Maserall Marcelin <maserallm@knights.ucf.edu>
2020-01-04 22:46:45 -0500

    jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,27 @@
 const mysql = require("mysql");
+let connection;
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burger_db"
-});
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "root",
+//   database: "burger_db"
+// });
+
+if (process.env.JAWSDB_URL) {
+  // DB is JawsDB on Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  // DB is local on localhost
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burger_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
